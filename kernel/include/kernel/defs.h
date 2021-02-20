6d9fc233998600c49e636208450d32219d616b38
Avinash Pai <paiavinash200@gmail.com>
2021-02-19 17:58:46 -0800

    gdt

---
+++ kernel/include/kernel/defs.h
@@ -0,0 +1,10 @@
+//
+// Created by Avinash Pai on 2/19/21.
+//
+
+#ifndef _DEFS_H
+#define _DEFS_H
+
+extern void gdt_install(void);
+
+#endif //_DEFS_H
--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
 #include <kernel/tty.h>
+#include <kernel/defs.h>
 
 extern const char _kernel_start[], _kernel_end[];
 
+void print_info(void) {
+    printf("[");
+    terminal_writestring_color("INFO", INFO);
+    printf("] ");
+}
+
 void kernel_main(void) {
+    gdt_install();
 	terminal_initialize();
 	printf("Initializing terminal...\n\n");
 	printf("\tWelcome to PaiOS!\n");
@@ -14,8 +22,6 @@ void kernel_main(void) {
            "    `'-.,_   '   _,.-'`\n"
            "      `\\  `\"\"\"\"\"`  /`\n"
            "        `\"\"-----\"\"`\n\n");
-	printf("[");
-	terminal_writestring_color("INFO", INFO);
-	printf("] ");
+	print_info();
     printf("Kernel loaded at [%x, %x]\n", _kernel_start, _kernel_end);
 }
